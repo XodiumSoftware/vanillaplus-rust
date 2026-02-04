d3ad9477620860ce7eb89cf2f810e36744da1203
Illyrius <FitimQ@live.nl>
2026-02-04 16:56:26 +0100

    refactor: update MOTD module to use server configuration
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/modules/motd.rs
+++ src/modules/motd.rs
@@ -1,3 +1,4 @@
+use pumpkin::server::Server;
 use serde::{Deserialize, Serialize};
 
 /// Represents a module handling motd mechanics within the system.
@@ -6,23 +7,24 @@ pub struct MotdModule {
 }
 
 impl MotdModule {
-    pub fn new(config: MotdModuleConfig) -> Self {
-        Self { config }
+    fn motd(&self, server: &mut Server) {
+        server.basic_config.motd = self.config.motd.join("\n")
     }
 }
 
 /// Represents the config of the module.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MotdModuleConfig {
-    pub join_msg: String,
-    pub leave_msg: String,
+    pub motd: Vec<String>,
 }
 
 impl Default for MotdModuleConfig {
     fn default() -> Self {
         Self {
-            join_msg: "Welcome, {player}!".to_string(),
-            leave_msg: "Goodbye, {player}!".to_string(),
+            motd: vec![
+                "<gradient:#CB2D3E:#EF473A><b>Ultimate Private SMP</b></gradient>".to_string(),
+                "<gradient:#FFE259:#FFA751><b>➤ WELCOME BACK LADS!</b></gradient>".to_string(),
+            ],
         }
     }
 }
